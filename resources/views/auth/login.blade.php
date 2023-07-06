e77b59a53b2130382c51397b437b17ee7798acbd
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2023-07-06 00:07:19 -0700

    push logi form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,260 +1,111 @@
-<!doctype html>
-
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
 
 <head>
-
+    <title>Hail A Taxi</title>
     <meta charset="utf-8">
-
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-
-    <!-- CSRF Token -->
-
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-
-    <!-- Fonts -->
-
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-
-    <link href="{{ asset('assets/plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-
-    <link href="{{ asset('assets/plugins/toast-master/css/jquery.toast.css')}}" rel="stylesheet">
-
-    <link href="{{ asset('css/colors/blue.css') }}" rel="stylesheet">
-
-    @yield('style')
-
-
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 
 <body>
+    <style>
+        body {
+            background: #288dfa;
+            position: relative;
+            height: 100vh;
+        }
 
-<style type="text/css">
-
-
-    .form-group.default-admin {
-        padding: 10px;
-        font-size: 14px;
-        color: #000;
-        font-weight: 600;
-        border-radius: 10px;
-        box-shadow: 0 0px 6px 0px rgba(0, 0, 0, 0.5);
-        margin: 20px 10px 10px 10px;
-    }
-
-    .form-group.default-admin .crediantials-field {
-        position: relative;
-        padding-right: 15px;
-        text-align: left;
-        padding-top: 5px;
-        padding-bottom: 5px;
-    }
-
-    .form-group.default-admin .crediantials-field > a {
-        position: absolute;
-        right: 0;
-        top: 0;
-        bottom: 0;
-        margin: auto;
-        height: 20px;
-    }
-
-    .login-register {
-        background-color: #072750;
-    }
-
-    .btn-primary, .btn-primary.disabled, .btn-primary:hover, .btn-primary.disabled:hover {
-        background: #072750;
-    }
-
-    <?php if(isset($_COOKIE['admin_panel_color'])){ ?>
-	a, a:hover, a:focus {
-        color: <?php echo $_COOKIE['admin_panel_color']; ?>;
-    }
-
-    .btn-primary, .btn-primary.disabled, .btn-primary:hover, .btn-primary.disabled:hover {
-        background: <?php echo $_COOKIE['admin_panel_color']; ?>;
-        border: 1px solid<?php echo $_COOKIE['admin_panel_color']; ?>;
-    }
-
-    [type="checkbox"]:checked + label::before {
-        border-right: 2px solid<?php echo $_COOKIE['admin_panel_color']; ?>;
-        border-bottom: 2px solid<?php echo $_COOKIE['admin_panel_color']; ?>;
-    }
-
-    .form-material .form-control, .form-material .form-control.focus, .form-material .form-control:focus {
-        background-image: linear-gradient(<?php echo $_COOKIE['admin_panel_color']; ?>, <?php echo $_COOKIE['admin_panel_color']; ?>), linear-gradient(rgba(120, 130, 140, 0.13), rgba(120, 130, 140, 0.13));
-    }
-
-    .btn-primary.active, .btn-primary:active, .btn-primary:focus, .btn-primary.disabled.active, .btn-primary.disabled:active, .btn-primary.disabled:focus, .btn-primary.active.focus, .btn-primary.active:focus, .btn-primary.active:hover, .btn-primary.focus:active, .btn-primary:active:focus, .btn-primary:active:hover, .open > .dropdown-toggle.btn-primary.focus, .open > .dropdown-toggle.btn-primary:focus, .open > .dropdown-toggle.btn-primary:hover, .btn-primary.focus, .btn-primary:focus, .btn-primary:not(:disabled):not(.disabled).active:focus, .btn-primary:not(:disabled):not(.disabled):active:focus, .show > .btn-primary.dropdown-toggle:focus {
-        background: <?php echo $_COOKIE['admin_panel_color']; ?>;
-        border-color: <?php echo $_COOKIE['admin_panel_color']; ?>;
-        box-shadow: 0 0 0 0.2rem<?php echo $_COOKIE['admin_panel_color']; ?>;
-    }
-
-    .login-register {
-        background-color: <?php echo $_COOKIE['admin_panel_color']; ?>;
-    }
-    <?php } ?>
-
-</style>
+        .form--user__icon span{
+            font-size: 32px;
+            position: absolute !important;
+            top: 50% !important;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
+        .container {
+                width: 40%;
+    margin: 0 auto;
+    background: green;
+    border-radius: 4px;
+    position: absolute;
+    top: 50%;
+    z-index: 2;
+    color: #fff;
+    content: '';
+    padding: 20px;
+    left: 50%;
+    transform: translate(-50%, -50%);
+        }
 
 
-<section id="wrapper">
+        .form--user__icon {
+                  border-radius: 50%;
+    height: 100px;
+    z-index: 9;
+    top: -50px;
+    text-align: center;
+    left: 50%;
+    transform: translate(-50%, 0);
+    position: absolute;
+    background: #ea7a1a;
+    width: 100px;
+    color: #fff;
+        }
 
+        button {
 
-    <div class="login-register">
+     width: 100%;
+     color:#fff !important;
+    border-radius: 50px !important;
+    border: 0 !important;
+    background: #003296 !important;
+        }
 
+        .container h3 {
+            margin-top: 60px;
+        }
+        form a{
+        color:#fff !important;
+        text-decoration:underline !important;
+        }
+    </style>
 
-        <div class="login-logo text-center py-3">
+    <div class="container">
+        <div class="form--user__icon mb-5">
 
-            <a href="#" style=""><img
-                        src="{{ asset('images/logo_login.png') }}" width="10%" height="10%"> </a>
+            <span>  <img src="{{ asset('images/hail.png') }}" style="height:150px !important;"> </span>
 
         </div>
+        <h3 class="text-center">Hail A Taxi </h3>
+        <h4 class="text-center  ">Login Form</h4>
+        <form  method="POST" action="{{ route('login') }}">
+            @csrf
+            <div class="form-group">
+                <label for="email"> <span class="glyphicon glyphicon-user"></span> Email:</label>
+                <input type="email" class="form-control  @error('email') is-invalid @enderror" id="email" placeholder="Enter email" name="email">
 
-        <div class="login-box card" style="margin-bottom:0%;">
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            <div class="form-group">
+                <label for="pwd"> <span class="glyphicon glyphicon-lock"></span> Password:</label>
+                <input type="password"  class="form-control  @error('password') is-invalid @enderror" id="password" placeholder="Enter password" name="password">
 
-
-            <div class="card-body">
-
-                <form class="form-horizontal form-material" method="POST" action="{{ route('login') }}">
-
-                    @csrf
-
-                    <div class="box-title m-b-20">{{ __('Login') }}</div>
-
-
-                    <div class="form-group ">
-
-
-                        <div class="col-xs-12">
-
-
-                            <input class="form-control" placeholder="{{ __('Email Address') }}" id="email" type="email"
-                                   class="form-control @error('email') is-invalid @enderror" name="email"
-                                   value="{{ old('email') }}" required autocomplete="email" autofocus></div>
-
-
-                        @error('email')
-
-                        <span class="invalid-feedback" role="alert">
-
-                                            <strong>{{ $message }}</strong>
-
-                                        </span>
-
-                        @enderror
-
-
-                    </div>
-
-
-                    <div class="form-group">
-
-
-                        <div class="col-xs-12">
-
-
-                            <input id="password" placeholder="{{ __('Password') }}" type="password"
-                                   class="form-control @error('password') is-invalid @enderror" name="password" required
-                                   autocomplete="current-password"></div>
-
-
-                        @error('password')
-
-                        <span class="invalid-feedback" role="alert">
-
-                                                <strong>{{ $message }}</strong>
-
-                                            </span>
-
-                        @enderror
-
-
-                    </div>
-
-
-                    <div class="form-group text-center m-t-20">
-
-
-                        <div class="col-xs-12">
-
-
-                            <input class="form-check-input" type="checkbox" name="remember"
-                                   id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-
-                            <label class="form-check-label" for="remember">
-
-                                {{ __('Remember Me') }}
-
-                            </label>
-
-
-                        </div>
-
-
-                    </div>
-
-
-                    <div class="form-group text-center m-t-20 mb-0">
-
-
-                        <div class="col-xs-12">
-
-
-                            <button type="submit"
-                                    class="btn btn-dark btn-lg btn-block text-uppercase waves-effect waves-light btn btn-primary">
-
-                                {{ __('Login') }}
-
-                            </button>
-
-                        </div>
-
-
-                    </div>
-
-                </form>
-
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
             </div>
 
-        </div>
-
+            <button type="submit" class="btn btn-default">Submit</button>
+        </form>
     </div>
-
-</section>
-
-<script src="{{asset('assets/plugins/jquery/jquery.min.js')}}"></script>
-
-<script src="{{ asset('assets/plugins/bootstrap/js/popper.min.js') }}"></script>
-
-<script src="{{ asset('assets/plugins/bootstrap/js/bootstrap.min.js') }}"></script>
-
-<script src="{{ asset('js/jquery.slimscroll.js') }}"></script>
-
-<script src="{{ asset('js/waves.js') }}"></script>
-
-<script src="{{ asset('js/sidebarmenu.js') }}"></script>
-
-<script src="{{ asset('assets/plugins/sticky-kit-master/dist/sticky-kit.min.js') }}"></script>
-
-<script src="{{ asset('assets/plugins/sparkline/jquery.sparkline.min.js')}}"></script>
-
-<script src="{{ asset('js/custom.min.js') }}"></script>
-
 
 </body>
 
