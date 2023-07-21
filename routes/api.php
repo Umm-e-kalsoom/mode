2ebd35fcaeeda2e5c8d8fb9f5183a07aa352b2ab
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2023-07-21 11:04:17 -0700

    oush

--- routes/api.php
+++ routes/api.php
@@ -316,10 +316,10 @@ Route::post('v1/resert-password/', [ResertPasswordController::class, 'resertPass
     Route::post('v1/token_pay/', [TokenPaymentController::class, 'token_pay']);
     Route::post('v1/remaining_token/', [TokenPaymentController::class, 'remain_token']);
     //Coupen
-    Route::post('v1/coupons/update/{id}', [CouponController::class, 'updateDiscount']);
-    Route::post('v1/coupons/store', 'CouponController@store_coupon');
-    Route::post('v1/get/coupons/{id}', [CouponController::class, 'get_coupen']);
-    Route::post('v1/coupens/{code}', [CouponController::class, 'coupen']);
+    Route::post('v1/coupons/update/{id}', [TokenPaymentController::class, 'updateDiscount']);
+    Route::post('v1/coupons/store', 'TokenPaymentController@store_coupon');
+    Route::post('v1/get/coupons/{id}', [TokenPaymentController::class, 'get_coupen']);
+    Route::post('v1/coupens/{code}', [TokenPaymentController::class, 'coupen']);
 
 Route::get('v1/wallet-history/', [DriverWalletHistoryController::class, 'getData']);
 
