c51f86b1e5a69c87bd88dc762e8ee1f4bf709228
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2023-07-28 03:00:17 -0700

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -268,7 +268,7 @@ class UserController extends Controller
             $user->save();
         }
 
-        return redirect('users');
+        return redirect()->back();
     }
 
     public function deleteUser($id)
