e658708ed7b032c78b15442d34416aa1969ae4a6
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2023-07-27 21:54:19 -0700

    Update UserLoginController.php

--- app/Http/Controllers/API/v1/UserLoginController.php
+++ app/Http/Controllers/API/v1/UserLoginController.php
@@ -80,7 +80,7 @@ class UserLoginController extends Controller
                             if (!empty($row)) {
                                 if($row['photo_path'] != '') {
                                     if (file_exists('assets/images/users' . '/' . $row['photo_path'])) {
-                                        $image_user = asset('my-assets/images/users') . '/' . $row['photo_path'];
+                                        $image_user = asset('assets/images/users') . '/' . $row['photo_path'];
                                     } else {
                                         $image_user = asset('assets/images/placeholder_image.jpg');
                                     }
