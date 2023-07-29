6b8e267acb309da461672b6a2c928c86f7eccbf5
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2023-07-29 01:22:10 -0700

    Update VehicleController.php

--- app/Http/Controllers/API/v1/VehicleController.php
+++ app/Http/Controllers/API/v1/VehicleController.php
@@ -35,9 +35,9 @@ class VehicleController extends Controller
     public function register(Request $request)
     {
 
-        $brand = $request->get('brand');
-        $prenom = str_replace("'", "\'", $brand);
-        $model = $request->get('model');
+        $brand = $request->get('brand_name');
+        // $prenom = str_replace("'", "\'", $brand);
+        $model = $request->get('model_name');
         $color = $request->get('color');
         $numberplate = $request->get('carregistration');
         $passenger = $request->get('passenger');
@@ -67,7 +67,7 @@ class VehicleController extends Controller
 //            DB::update('update tj_conducteur set model=?',[$vehicleModel->id]);
                 $row = $chkid->toArray();
                 $id_vehicule = $row['id'];
-                $updatedata = DB::update('update tj_vehicule set brand = ?,model = ?,passenger = ?,$package_size = ?,$package_weight = ?,$num_of_pets = ?,$num_of_luggage = ?,num_of_petscolor = ?,numberplate = ?,modifier = ?,id_type_vehicule = ?,car_make = ?,
+                $updatedata = DB::update('update tj_vehicule set brand_name = ?,model_name = ?,passenger = ?,$package_size = ?,$package_weight = ?,$num_of_pets = ?,$num_of_luggage = ?,num_of_petscolor = ?,numberplate = ?,modifier = ?,id_type_vehicule = ?,car_make = ?,
                        km = ?,milage = ? where id = ?', [$brand, $vehicleModel->id, $passenger, $color, $numberplate, $date_heure, $id_categorie_vehicle, $car_make, $km, $milage, $id_vehicule]);
 
                 if (!empty($updatedata)) {
@@ -93,7 +93,7 @@ class VehicleController extends Controller
                 }
 //                DB::update('update tj_conducteur set model=?',[$vehicleModel->id]);
                 $insertdata = DB::insert("insert into tj_vehicule(passenger,package_size,package_weight,num_of_luggage,num_of_pets,brand,model,color,numberplate,id_conducteur,statut,creer,updated_at,id_type_vehicule,car_make,milage,km)
-	        values('" . $passenger . "','" . $package_size . "','" .$package_weight. "','".$num_of_luggage."','".$num_of_pets."','" . $brand . "','" . $vehicleModel->id . "','" . $color . "','" . $numberplate . "','" . $id_driver . "','yes','" .
+	        values('" . $passenger . "','" . $package_size . "','" .$package_weight. "','".$num_of_luggage."','".$num_of_pets."','" . $brand . "','" . $model . "','" . $color . "','" . $numberplate . "','" . $id_driver . "','yes','" .
                     $date_heure . "','" . $date_heure . "','" . $id_categorie_vehicle . "','" . $car_make . "','" . $milage . "','" . $km . "')");
                 $id = DB::getPdo()->lastInsertId();
                 if ($id > 0) {
