d872963250ba2eb7edcb0f1b9270ef17301c0d90
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2023-07-30 11:12:39 -0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -324,7 +324,7 @@ Route::post('v1/resert-password/', [ResertPasswordController::class, 'resertPass
 
     Route::post('v1/destination', [DriverController::class, 'destination']);
     Route::get('v1/ride_settings', [TokenPaymentController::class, 'settings']);
-    Route::get('v1/check_balance', [TokenPaymentController::class, 'check_balance']);
+    Route::post('v1/check_balance', [TokenPaymentController::class, 'check_balance']);
 
 Route::get('v1/wallet-history/', [DriverWalletHistoryController::class, 'getData']);
 
