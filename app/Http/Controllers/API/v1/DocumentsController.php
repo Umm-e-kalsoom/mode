de6da42ea30816486b31aaac6dd54acfba5f1725
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2023-08-08 11:05:04 -0700

    Update DocumentsController.php

--- app/Http/Controllers/API/v1/DocumentsController.php
+++ app/Http/Controllers/API/v1/DocumentsController.php
@@ -148,12 +148,12 @@ class DocumentsController extends Controller
 
             $document_name = DB::table('admin_documents')->where('id',$document_id)->first();
             $filename = str_replace(' ','_',$document_name->title) . '_' . time() . '.' . $extenstion;
-            $file->move('assets/images/driver/documents/', $filename);
+            $file->move('/public/assets/images/driver/documents/', $filename);
 
             $get_driver_document = DB::table('driver_document')->where('document_id',$document_id)->where('driver_id',$driver_id)->first();
 			if($get_driver_document){
 				if(file_exists('assets/images/driver/documents' . '/' . $get_driver_document->document_path)) {
-	                unlink('assets/images/driver/documents' . '/' . $get_driver_document->document_path);
+	                unlink('/public/assets/images/driver/documents' . '/' . $get_driver_document->document_path);
 	            }
 				$driver_document = DriversDocuments::find($get_driver_document->id);
 				$driver_document->document_path = $filename;
